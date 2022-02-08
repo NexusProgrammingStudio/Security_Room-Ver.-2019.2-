9060b85186521ffce036594af36eee41bdde60ed
NexusProgrammingStudio <60765914+NexusProgrammingStudio@users.noreply.github.com>
2022-02-08 13:26:06 +0530

    Initial commit

---
+++ Assets/Sample Game/Scenes/Scripts/Audio_Script.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Audio_Script : MonoBehaviour
+{
+    public AudioSource Voice;
+    private MeshRenderer MyRender;
+
+    private bool Cooldown = false;
+
+    void Start()
+    {
+        MyRender = GetComponent<MeshRenderer> ();
+        Voice = GetComponent<AudioSource> ();
+    }
+
+    public void GazedAt(bool gazing)
+    {
+        if(gazing && Cooldown != true)
+        {
+            Voice.Play();
+            Cooldown = true;
+        }
+        else
+        {
+            Invoke("Cooling_Time",30);
+        }
+    }
+
+    void Cooling_Time()
+    {
+        Cooldown = false;
+    }
+}
+
